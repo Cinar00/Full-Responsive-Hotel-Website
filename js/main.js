34221964e3b5d364f1838c636866e18ef91e20c9
Çınar <bytllhcinar@gmail.com>
2022-10-07 19:03:24 +0300

    Responsive_Hotel_Website

---
+++ js/main.js
@@ -0,0 +1,77 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+};
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a) {
+    a.addEventListener("click", function() {
+        navCollapse.classList.remove("show");
+    });
+});
+
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
+
+// Counter Design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+});
+
+// Our Partner
+var swiper = new Swiper(".our-partner", {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        991: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+        767: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 8,
+        },
+    },
+});
